258f69dd7adcd0ee39a6047156c9de6d11f5251c
AaronW <zmtzwb@gmail.com>
2020-02-17 02:18:14 +0800

    init antd

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.directive('permission', {
+  inserted: function (el, binding) {
+    const {value} = binding;
+    if (value !== 'create') {
+      el.parentNode.removeChild(el);
+    }
+  }
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <Button v-permission="permission">Create</Button>
+    <Button type="danger">Button</Button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -9,10 +11,17 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import {Button} from 'ant-design-vue'
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      permission: 'delete'
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld, Button
   }
 }
 </script>
