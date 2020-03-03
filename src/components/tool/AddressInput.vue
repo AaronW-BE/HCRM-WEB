d7e8f4c046452f5bb0bf900fa2f96ddbc99f0880
aaronwb <zmtzwb@gmail.com>
2020-03-03 17:54:41 +0800

    完成地址选择器封装

--- src/components/tool/AddressInput.vue
+++ src/components/tool/AddressInput.vue
@@ -1,12 +1,77 @@
 <template>
     <div>
-        <a-cascader></a-cascader>
+        <a-input-group>
+            <a-cascader
+                    v-model="selectedAddress"
+                    :options="address"
+                    @change="onAddressSelected"
+            ></a-cascader>
+            <a-input placeholder="请输入详细地址" v-model="detail" v-if="showDetail" @blur="onDetailBlur" />
+        </a-input-group>
     </div>
 </template>
 
 <script>
+    import {address} from "@/assets/js/cities";
+
     export default {
-        name: "AddressInput"
+        name: "AddressInput",
+        props: {
+            value: {
+
+            },
+            showDetail: {
+                type: Boolean,
+                default: true
+            }
+        },
+        data() {
+            return {
+                address,
+                selectedAddress: [],
+                detail: ''
+            }
+        },
+        mounted() {
+            console.log(this.value);
+            const addressArr = this.value.split(',');
+            if (this.showDetail) {
+                if (addressArr.length >= 3) {
+                    this.selectedAddress = addressArr.slice(0, addressArr.length);
+                    this.detail = addressArr[addressArr.length - 1];
+                } else {
+                    throw new Error("address initialValue value is error!");
+                }
+            } else {
+                if (addressArr.length >= 2) {
+                    console.log(addressArr, addressArr.length);
+                    this.selectedAddress = addressArr.slice(0, addressArr.length);
+                } else {
+                    throw new Error("address initialValue value is error!");
+                }
+            }
+
+        },
+        methods: {
+            onAddressSelected(value) {
+                this.selectedAddress = value;
+                if (!this.showDetail) {
+                    this.$emit("change", value.join(','));
+                }
+                if (this.detail) {
+                    this.onDetailBlur();
+                }
+            },
+            onDetailBlur() {
+                let result = [
+                    ...this.selectedAddress,
+                    this.detail
+                ];
+                if (this.selectedAddress.length && this.detail) {
+                    this.$emit('change', result.join(','));
+                }
+            }
+        }
     }
 </script>
 
