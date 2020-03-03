a3b2d79e39fca587b8d67942af1537da1c41b517
aaronwb <zmtzwb@gmail.com>
2020-03-03 17:02:09 +0800

    添加地址组件

---
+++ src/components/tool/AddressInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <a-cascader></a-cascader>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AddressInput"
+    }
+</script>
+
+<style scoped>
+
+</style>
