2e6191794acc27eb34daa81639c09e502fb0bc82
aaronwb <zmtzwb@gmail.com>
2020-03-04 16:15:44 +0800

    订单详情增加创建人信息，及客户关联

--- src/views/Order/OrderDetail.vue
+++ src/views/Order/OrderDetail.vue
@@ -11,19 +11,22 @@
                         <item term="订单来源">{{detail.original}}</item>
                         <item term="订单金额">{{detail.orderAmount}}</item>
                     </data-detail-list>
+
+                    <data-detail-list>
+                        <item term="关联客户">
+                            <a @click="$router.push({name: 'customerDetail', params: {id: detail.creatorId}})" >{{detail.customer}}</a>
+                        </item>
+                        <item term="创建人">{{detail.creator}}</item>
+
+                        <item term="备注">{{detail.orderRemark}}</item>
+                        <item term="订单创建时间">{{ new Date(detail.createAt).toLocaleString() }}</item>
+                    </data-detail-list>
                     <a-divider></a-divider>
 
                     <data-detail-list title="快递信息">
                         <item term="快递公司">{{detail.orderExpressCorp}}</item>
                         <item term="快递单号">{{detail.orderExpressNo}}</item>
                         <item term="订单地址">{{detail.orderAddress}}</item>
-                    </data-detail-list>
-
-                    <a-divider></a-divider>
-                    <data-detail-list title="其他信息">
-                        <item term="备注">{{detail.orderRemark}}</item>
-                        <item term="订单创建时间">{{ new Date(detail.createAt).toLocaleString() }}</item>
-
                     </data-detail-list>
                 </div>
             </a-skeleton>
