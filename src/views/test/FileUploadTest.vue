affaa2f279b025bbc24c15887e3be211a91485ad
aaronwb <zmtzwb@gmail.com>
2020-03-06 12:04:29 +0800

    新增文件上传测试

---
+++ src/views/test/FileUploadTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="file" @change="fileChange"/>
+    </div>
+</template>
+
+<script>
+
+    import {API} from "../../api";
+    import {TestFileUpload} from "../../api/template";
+
+    export default {
+        name: "FileUploadTest",
+        methods: {
+            fileChange(e) {
+                let file = e.target.files[0];
+                API(TestFileUpload, {
+                    name: 'file',
+                    file: file
+                }).then(res => {
+                    console.log(res);
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
